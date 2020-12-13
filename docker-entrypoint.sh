a15e0e8c271b5839703430e1821f035a1490af74
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2020-12-13 18:30:44 +0530

    docker entrypoint updated to install packages 📦 docker entrypoint install packages if node_modules does not exist

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/sh
 
+if [[ ! -d node_modules ]]; then
+    yarn install
+
+    yarn add -D eslint eslint-config-airbnb eslint-config-prettier eslint-plugin-import \
+    eslint-plugin-node eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks \
+    node-sass@4.14.1 prettier prop-types
+fi
+
 yarn start
